529bf6611814835873cf1e8301ff8a6fbe036514
mohammad7997 <mjarahi1379@gmail.com>
2022-04-05 15:32:00 +0430

    complete register and verify email

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,21 +4,31 @@
 <div class="col-md-12 col-xl-7 chat"><div class="card mb-sm-3 mb-md-0 contacts_card">
     <div class="row">
         <div class="container col-11 mt-5">
-            <form id="register">
-
+            <form id="register" method="POST" action="{{ route('register') }}">
+                @csrf
                 <div class="row">
 
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                        <label class="form-label" for="firstName">First Name</label>
-                        <input type="text" id="firstName" class="form-control " />
+                        <label class="form-label" for="first_name">First Name</label>
+                        <input type="text" id="first_name" name="first_name" class="form-control"  value="{{ old('first_name') }}"/>
+                        @error('first_name')
+                            <div class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
                     </div>
                   </div>
 
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                        <label class="form-label" for="lastName">Last Name</label>
-                        <input type="text" id="lastName" class="form-control" />
+                        <label class="form-label" for="last_name">Last Name</label>
+                        <input type="text" id="last_name" name="last_name" class="form-control" value="{{ old('last_name') }}"/>
+                        @error('last_name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
                   </div>
 
@@ -28,15 +38,25 @@
 
                   <div class="col-md-6 mb-4 pb-2">
                     <div class="form-outline">
-                        <label class="form-label" for="emailAddress">Email</label>
-                        <input type="email" id="emailAddress" class="form-control " />
+                        <label class="form-label" for="email">Email</label>
+                        <input type="email" id="email" name="email" class="form-control " value="{{ old('email') }}"/>
+                        @error('email')
+                            <div class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
                     </div>
                   </div>
 
                   <div class="col-md-6 mb-4 pb-2">
                     <div class="form-outline">
-                        <label class="form-label" for="phoneNumber">User name</label>
-                        <input type="tel" id="phoneNumber" class="form-control " />
+                        <label class="form-label" for="user_name">User name</label>
+                        <input type="text" id="user_name" name="user_name" class="form-control " value="{{ old('user_name') }}"/>
+                        @error('user_name')
+                            <div class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
                     </div>
                   </div>
 
@@ -46,15 +66,25 @@
 
                   <div class="col-md-6 mb-4 pb-2">
                     <div class="form-outline">
-                        <label class="form-label" for="emailAddress">Password</label>
-                        <input type="email" id="emailAddress" class="form-control " />
+                        <label class="form-label" for="password">Password</label>
+                        <input type="password" id="password" name="password" class="form-control " value="{{ old('password') }}"/>
+                        @error('password')
+                            <div class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
                     </div>
                   </div>
 
                   <div class="col-md-6 mb-4 pb-2">
                     <div class="form-outline">
-                        <label class="form-label" for="phoneNumber">Password Configuration</label>
-                        <input type="tel" id="phoneNumber" class="form-control " />
+                        <label class="form-label" for="password_confirmation">Password Configuration</label>
+                        <input type="password" id="password_confirmation" name="password_confirmation" class="form-control " value="{{ old('password_confirmation') }}"/>
+                        @error('password')
+                            <div class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </div>
+                        @enderror
                     </div>
                   </div>
 
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -25,4 +25,36 @@
         </div>
     </div>
 </div>
+@endsection --}}
+
+@extends('layout')
+@section('content')
+
+<div class="col-md-12 col-xl-7 chat"><div class="card mb-sm-3 mb-md-0 contacts_card">
+    <div class="container col-10">
+        <div class="row text-center" id="row_email">
+            <div class="w-100" id="emial_confirmation_header">{{ __('Verify Your Email Address') }}</div>
+            <div class="card-body">
+                @if (session('resent'))
+                    <div class="alert alert-success" role="alert">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </div>
+                @endif
+
+                <p id="emial_confirmation_content">
+                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('If you did not receive the email') }},
+                </p>
+                <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <input class="btn btn-primary btn-lg " type="submit" value={{ __('click here to request another') }}>
+                    {{-- <button type="submit" class="btn btn-danger p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>. --}}
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+
 @endsection
